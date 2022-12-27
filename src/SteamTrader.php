5dc2044869f6b206dd950edf7341727cbb30ff96
Daniel Maga <daniel.maga@codefactorygroup.com>
2022-12-27 15:04:38 +0000

    second

--- src/SteamTrader.php
+++ src/SteamTrader.php
@@ -10,13 +10,14 @@ class SteamTrader
      * This package is a wrapper for steam
      *
      * @param string $sessionId
-     * @param string $cookies
+     * @param array $cookies
+     * @param string $apiKey
      * @return SteamTrade
      */
-    public static function get(string $sessionId, string $cookies): SteamTrade
+    public static function get(string $sessionId, array $cookies, string $apiKey = ''): SteamTrade
     {
         $trade = new SteamTrade();
-        $trade->setup($sessionId, $cookies);
+        $trade->setup($sessionId, $cookies, $apiKey);
 
         return  $trade;
     }
